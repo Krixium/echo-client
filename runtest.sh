35e89063c9a96ac211617da3f7813a28c959592e
Benny Wang <bennywang75@hotmail.com>
2019-02-22 17:22:09 -0800

    Added test runner script

---
+++ runtest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1: host
+# 2: number of instances
+# 3: port
+# 4: packet size
+# 5: number messages
+# 6: delay
+
+make clean
+make
+
+for i in {1..$2}
+do
+    ./echoc.out $1 $3 asdf $4 $5 $6
+done
